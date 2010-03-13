5a53f260d87e47a2e6bde6c9e072179f2bfa36f3
Andrew Hutchings <andrew.hutchings@gmail.com>
2010-03-12 21:59:42 -0500

    Update single.php and whitespace changes in home.php.

--- home.php
+++ home.php
@@ -1,35 +1,35 @@
 <?php $theme->display('header') ?>
 
     <div id="coreContent" class="hfeed">
-      
-    	<?php if (count($posts)): ?>
 
-    		<?php foreach ($posts as $post): ?>
+        <?php if (count($posts)): ?>
+
+            <?php foreach ($posts as $post): ?>
 
       <div class="post hentry">
-		<h5 class="postDate"><abbr class="published">
-		<?php echo $post->pubdate->text_format('{F} {j}, {Y}') ?>
-		</abbr></h5>
+        <h5 class="postDate"><abbr class="published">
+        <?php echo $post->pubdate->text_format('{F} {j}, {Y}') ?>
+        </abbr></h5>
         <div class="postContent">
           <h3 class="entry-title"><a href="<?php echo $post->permalink ?>" rel="bookmark"><?php echo $post->title_out ?></a></h3>
           <!-- <h4 class="vcard author">by <span class="fn"><?php echo $post->author->username ?></span></h4> -->
-          
+
           <div class="entry-content">
-			
+
               <?php the_content('Read the rest of this entry &raquo;'); ?>
 
           </div>
         </div>
         <div class="postMeta">
-        
+
         <?php if ('closed' == $post->comment_status) : ?>
 
           <div class="comments closed">
 
-         	<?php else : ?>
-        
+             <?php else : ?>
+
           <div class="comments">
-        
+
           <?php endif; ?>
 
             <?php comments_popup_link('leave a comment', '1 comment', '% comments', '', 'comments closed'); ?>
@@ -37,19 +37,19 @@
         </div>
       </div>
 
-		<?php endforeach ?>
+        <?php endforeach ?>
 
     <div class="pageNav">
       <div class="prev"><?php next_posts_link('&laquo; Older') ?></div>
       <div class="next"><?php previous_posts_link('Newer &raquo;') ?></div>
     </div>
 
-	<?php else : ?>
+    <?php else : ?>
 
-		<h2>Not Found</h2>
-		<p>Sorry, but you are looking for something that isn't here.</p>
+        <h2>Not Found</h2>
+        <p>Sorry, but you are looking for something that isn't here.</p>
 
-	<?php endif; ?>
+    <?php endif; ?>
 
   </div>
 
--- single.php
+++ single.php
@@ -2,54 +2,46 @@
 
 <div id="coreContent">
 
-	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-
       <div class="post single hentry">
         <div class="postContent">
-          <h3 class="entry-title"><?php the_title(); ?></h3>
-          <h4 class="vcard author">by <span class="fn"><?php the_author(); ?></span></h4>
+          <h3 class="entry-title"><?php echo $post->title_out ?></h3>
+          <h4 class="vcard author">by <span class="fn"><?php echo $post->author->username ?></span></h4>
           <div class="entry-content">
-              <?php the_content(); ?>
+              <?php echo $post->content_out ?>
           </div>
         </div>
         <div class="postMeta">
           <?php
-          $arc_year = get_the_time('Y');
-          $arc_month = get_the_time('m');
-          $arc_day = get_the_time('d');
+          $arc_args = array(
+            'year' => $post->pubdate->text_format('{Y}'),
+            'month' => $post->pubdate->text_format('{m}'),
+            'day' => $post->pubdate->text_format('{d}')
+          );
           ?>
-        
-          <div class="postDate"><span>Published:</span> <abbr class="published" title="<?php the_time('Y-m-d\TH:i:sO'); ?>"><a href="<?php echo get_day_link("$arc_year", "$arc_month", 
-          "$arc_day"); ?>"><?php the_time('F j, Y'); ?></a></abbr></div>
-        	<div class="categories"><span>Filed Under:</span> <?php the_category(', '); ?></div>
-        	<?php the_tags('<span>Tags:</span> ', ' : ', ''); ?>
+
+          <div class="postDate"><span>Published:</span> <abbr class="published" title="<?php echo $post->pubdate->text_format('{Y}-{m}-{d}T{H}:{i}:{s}{O}') ?>">
+            <a href="<?php URL::out('display_entries_by_date', $arc_args) ?>"><?php echo $post->pubdate->text_format('{F} {j}, {Y}'); ?></a></abbr></div>
+            <div class="categories"><span>Filed Under:</span> <?php the_category(', '); ?></div>
+            <?php the_tags('<span>Tags:</span> ', ' : ', ''); ?>
         </div>
       </div>
 
-      <?php 
+      <?php
       /*
       <div class="googleAd">
-        
+
         <!-- You Ad Code Here -->
 
       </div>
       */
       ?>
-      
-	<?php comments_template(); ?>
+
+    <?php $theme->display('comments') ?>
 
   <div class="pageNav">
     <div class="prev"><?php previous_post_link('%link', '&laquo; Previous Post'); ?></div>
     <div class="next"><?php next_post_link('%link', 'Next Post &raquo;') ?></div>
   </div>
-
-
-	<?php endwhile; else: ?>
-
-		<p>Sorry, no posts matched your criteria.</p>
-
-<?php endif; ?>
 
 </div>
 
